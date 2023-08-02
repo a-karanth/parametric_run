5f752bbdfe7984dbe236dc54cb33df03f2ede10a
a-karanth <arundhati.karanth@gmail.com>
2023-08-02 13:35:23 +0200

    converted sample dataframe to numpy array for morris.analyse()

--- src/sobol_method.py
+++ src/sobol_method.py
@@ -35,7 +35,6 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 from matplotlib.ticker import AutoMinorLocator
 import matplotlib.ticker as ticker
-import seaborn as sns
 import numpy as np
 import pandas as pd
 from collections import OrderedDict
@@ -44,24 +43,27 @@ from collections import OrderedDict
 directory = (os.path.dirname(os.path.realpath(__file__)))
 sample_folder = '\\res'
 
+output_file1 = 'morris_sample_st3'
+output_file2 = 'morris_sample_pvt3'
+
 #create one list for each variable indicating their input values
 input_st = {'volume' : [0.1, 0.2, 0.3, 0.4],
               'coll_area': [4, 8, 16,20],
-              'flow_rate': [50, 100, 200]}
-               # 'design_case': ['ST']}
+              'flow_rate': [50, 100, 200],
+              'r_level': ['r0','r1']}
 
 input_pvt = {'volume' : [0.1, 0.2, 0.3, 0.4],
               'coll_area': [4, 8, 16,20],
-              'flow_rate': [50, 100, 200]}
-              # 'design_case': ['PVT']}
+              'flow_rate': [50, 100, 200],
+              'r_level': ['r0','r1']}
 
 
 list_design_case_pvt_batt = ['PVT_Batt_6', 'PVT_Batt_9']
 
 #%% Function for creating bounds and scenarios
-# define problem to be analysed (number, name and range of the variables)
-# creating the problem based on list index numbers. Which will then be used to 
-# create final sample by applying to the lists declared above
+#   define problem to be analysed (number, name and range of the variables)
+#   creating the problem based on list index numbers. Which will then be used to 
+#   create final sample by applying to the lists declared above
 
 def cal_bounds_scenarios(dct):
     key = list(dct.keys())
@@ -89,15 +91,15 @@ problem_pvt = {
 
 
 #%% Create the sample by defining the N value. samples contain index values for 
-# the parameter lists. As the calculation for second order is defined as false, 
-# the following equation will be used to defined the sample size: N∗(D+2) .
-# As the values defined by Sobol and Morris are floats, they need to be rounded 
-# and converted to int in order to be used as indices
+#   the parameter lists. As the calculation for second order is defined as false, 
+#   the following equation will be used to defined the sample size: N∗(D+2) .
+#    As the values defined by Sobol and Morris are floats, they need to be rounded 
+#   and converted to int in order to be used as indices
 
 # param_values = saltelli.sample(problem, N, calc_second_order=True)
 # sobol_sample = param_values.round()
 
-N = 32
+N = 2
 morris_sample_st = morris.sample(problem_st, N, num_levels=4, optimal_trajectories=None)
 morris_sample_st = morris_sample_st.round().astype(int)
 
@@ -127,11 +129,61 @@ st_out = assign_indices(morris_sample_st, input_st, 'ST')
 pvt_out = assign_indices(morris_sample_pvt, input_pvt, 'PVT')
 
 os.chdir(directory+sample_folder)
-st_out.to_csv('morris_st_sample'+'.csv', index=False)
-pvt_out.to_csv('morris_pvt_sample'+'.csv', index=False)
+# st_out.to_csv(output_file1+'.csv', index=False)
+# pvt_out.to_csv(output_file2+'.csv', index=False)
+
+
+#%% fill existing results
+trn_folder = 'trn\\'
+results = pd.read_csv('sim_results.csv', index_col='label')
+results['total_costs'] = results['el_bill']+results['gas_bill']
+results['total_emission'] = (results['el_em']+results['gas_em'])/1000
+
+existing = pd.read_csv(trn_folder+'list_of_inputs.csv',header=0, index_col='label').sort_values(by='label')
+dfresults = pd.concat([existing, results],axis=1)
+
+st1 = pd.merge(st_out,dfresults, how='left')
+pvt1 = pd.merge(pvt_out,dfresults, how='left')
+
+#%% flag missing data
+missing_st = st1[np.isnan(st1['el_bill'])]
+missing_pvt = pvt1[np.isnan(pvt1['el_bill'])]
+
+#%% dropping design case column since it did not exist in problem defintion
+#   Representing r_level in with corresponding digit
+#   Converting to numpy array of floats because that is the required input for morris.analyze()
+
+analyse_sample = st_out.drop('design_case', axis=1)
+analyse_sample['r_level'] = (analyse_sample['r_level'].str.extract('(\d+)'))
+analyse_sample = analyse_sample.to_numpy(dtype=float)
+#%%
+from SALib.analyze import morris
+Si = morris.analyze(
+    problem_st,
+    analyse_sample,
+    np.ravel(st1['el_bill']),
+    conf_level=0.95,
+    print_to_console=True,
+    num_levels=4,
+    num_resamples=100,
+)
+
+#%% plots from SALib git hub
+
+from SALib.plotting.morris import (
+    horizontal_bar_plot,
+    covariance_plot,
+    sample_histograms,
+)
+fig, (ax1, ax2) = plt.subplots(1, 2)
+horizontal_bar_plot(ax1, Si, {}, sortby="mu_star", unit=r"EUR/year")
+covariance_plot(ax2, Si, {}, unit=r"EUR/year")
+
+fig2 = plt.figure()
+sample_histograms(fig2, analyse_sample, problem_st, {"color": "y"})
+plt.show()
 
 #%%
-
 # #Give names to the file containing all the simulation outputs (DS_results.csv) and 
 # #the one containing the Sobol sample (SA_sample.csv)
 # os.chdir(results_SAn_DA)
