2b18861a8eb1b0cb187040ee0b59274358275e4c
a-karanth <arundhati.karanth@gmail.com>
2023-10-27 12:36:38 +0200

    added lines for modifyig infiltration rates

--- src/ModifyType56.py
+++ src/ModifyType56.py
@@ -38,6 +38,7 @@ class ModifyType56:
     cm3 = '0.03\t'
     cm5 = '0.05\t'
     cm6 = '0.06\t'
+    cm7 = '0.07\t'
     cm10 = '0.1\t'
     cm15 = '0.15\t'
     cm20 = '0.2\t'
@@ -50,7 +51,7 @@ class ModifyType56:
     r0_wall_thk = cm10 + cm0 + cm10
     r0_wall = layer + r0_wall_con + '\n' + thickness + r0_wall_thk + '\n'
     r0_roof_con = plasterboard+ air_cav + timber + roofdeck + rooftile
-    r0_roof_thk = cm3 + cm0 + cm5 + cm1 + cm2
+    r0_roof_thk = cm2 + cm0 + cm7 + cm3 + cm3
     r0_roof = layer + r0_roof_con + '\n' + thickness + r0_roof_thk + '\n'
     r0_floor_con = concrete + air_cav_floor
     r0_floor_thk = cm25 + cm0
@@ -77,7 +78,7 @@ class ModifyType56:
         FLOWTOAIRNODE=1 : PERT=0 : PENRT=0 : RADMATERIAL=undefined : RADMATERIAL_SHD1=undefined\n'
     
     r2_wall_con = brick + air_cav + pu + brick
-    r2_wall_thk = cm10 + cm0 + cm6 + cm10
+    r2_wall_thk = cm10 + cm0 + cm10 + cm10
     r2_wall = layer+r2_wall_con +'\n' + thickness+r2_wall_thk + '\n'
     r2_roof_con = plasterboard+ air_cav + pu + timber + roofdeck + rooftile
     r2_roof_thk = cm3 + cm0 + cm15 + cm5 + cm1 + cm2
@@ -92,13 +93,18 @@ class ModifyType56:
         FLOWTOAIRNODE=1 : PERT=0 : PENRT=0 : RADMATERIAL=undefined : RADMATERIAL_SHD1=undefined\n'
     
   
-    def change_r(self, house_file, scenario):
+    def change_r(self, house_file, scenario, inf):
         with open(house_file, 'r') as file_in:
             filedata = file_in.readlines()
         old_ext_wall = "CONSTRUCTION EXT_WALL\n"
+        old_inf = "INFILTRATION INFIL1\n"
         
         for line in filedata:
-            if scenario == 'r0' and line == old_ext_wall:
+            if line == old_inf:
+                filedata[filedata.index(line)+1] = " AIRCHANGE=" + str(inf) + '\n'
+                print(filedata[filedata.index(line)+1])
+            
+            elif scenario == 'r0' and line == old_ext_wall:
                 filedata[filedata.index(line)+1] = self.r0_wall
                 del filedata[filedata.index(line)+2]
                 filedata[filedata.index(line)+6] = self.r0_roof
