437098bceddfd6673c8deec919a83f4cd50fe86c
a-karanth <arundhati.karanth@gmail.com>
2023-11-03 17:36:49 +0100

    checking the results in one file

--- src/check_one_file.py
+++ src/check_one_file.py
@@ -9,7 +9,7 @@ import sys
 import time                 # to measure the computation time
 import os 
 dir_trial = os.getcwd()
-os.chdir('..')
+#os.chdir('..')
 dir_main = os.getcwd()
 dir_lib = dir_main + '\\src'
 sys.path.append(dir_lib)
@@ -29,13 +29,33 @@ directory = os.path.dirname(os.path.realpath(__file__))+'\\'
 folder = 'res\\trn\\'
 # directory = 'C:\\Users\\20181270\\OneDrive - TU Eindhoven\PhD\\TRNSYS\\Publication1\\'
 # folder = 'Restart\\'
-file = '197_cp'
+file = '840_cp'
 prefix = directory + folder + file
 
-# controls, energy, temp_flow = pf.create_dfs(prefix)   #use for regular
-controls, energy, temp_flow, energy_monthly, energy_annual, rldc, ldc = pf.cal_base_case(prefix) # use for base case
+t_start = datetime(2001,1,1, 0,0,0)
+t_end = datetime(2002,1,1, 0,0,0)
+
+if 'cp' in file:
+    controls, energy, temp_flow = pf.cal_base_case(prefix)
+    
+else:
+    temp_flow = pd.read_csv(prefix+'_temp_flow.txt', delimiter=",",index_col=0)
+    energy = pd.read_csv(prefix+'_energy.txt', delimiter=",", index_col=0)
+    controls = pd.read_csv(prefix+'_control_signal.txt', delimiter=",",index_col=0)
+    
+    controls = pf.modify_df(controls, t_start, t_end)
+    temp_flow = pf.modify_df(temp_flow, t_start, t_end)
+    energy = pf.modify_df(energy, t_start, t_end)/3600     # kJ/hr to kW 
+    energy = pf.cal_energy(energy, controls)
+
 energy_monthly, energy_annual = pf.cal_integrals(energy)
+
 el_bill, gas_bill = pf.cal_costs(energy)
+el_em, gas_em = pf.cal_emissions(energy)
+pl,pe = pf.peak_load(energy)
+rldc,ldc = pf.cal_ldc(energy)
+opp_im, opp_ex, import_in, export_in = pf.cal_opp(rldc)
+# penalty, energy = pf.cal_penalty(energy)
 
 #%% plots
 pt = Plots(controls, energy, temp_flow)
@@ -43,21 +63,20 @@ t1 = datetime(2001,1,1, 0,0,0)
 t2 = datetime(2001,1,8, 0,0,0)
 pt.plot_q(t1, t2)
 
-#%% long method
+#%% plotlt plot for residual LDC
+import plotly, plotly.graph_objects as go, plotly.offline as offline, plotly.io as pio
+from plotly.subplots import make_subplots
+pio.renderers.default = 'browser'
+fig = make_subplots(rows=1)
 
-# temp_flow = pd.read_csv(prefix+'_temp_flow.txt', delimiter = ",", index_col=0)
-# energy = pd.read_csv(prefix+'_energy.txt', delimiter = ",", index_col=0)
-# controls = pd.read_csv(prefix+'_control_signal.txt', delimiter = ",", index_col=0)
+color_scale = plotly.colors.sequential.Viridis
+color_scale = plotly.colors.qualitative.Bold
 
-# controls = pf.modify_df(controls, t_start, t_end)
-# temp_flow = pf.modify_df(temp_flow, t_start, t_end)
-# energy = pf.modify_df(energy, t_start, t_end)/3600     # kJ/hr to kW 
-# energy = pf.cal_energy(energy, controls)
-
-# energy['Qheat'] = energy['Qheat_living1']+energy['Qheat_living2']+energy['Qheat_bed1']+energy['Qheat_bed2']
-# energy['Qhp4sh'] = energy['Qheat']/2.8
-# energy['Qhp4tank'] = energy['Qaux_dhw']/1.9
-# energy['Qaux_dhw'] = 0
-# energy['Qload'] = energy['Qdev']+energy['Qltg']+energy['Qhp4sh']+energy['Qhp4tank']
-# energy['Qfrom_grid'] = energy['Qload']
-
+data = rldc['net_import']
+fig.add_trace(go.Scatter(x=rldc.index, 
+                          y=data, 
+                          text=rldc['timestamp'],
+                          hoverinfo="y+text"
+                          )
+               )
+fig.show()
